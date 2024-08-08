fbad5e94bd613bdf4a58965c36edb449fc2ca799
shivangi980 <113875231+shivangi980@users.noreply.github.com>
2024-08-08 12:39:51 +0530

    resident_model

---
+++ app/Filament/Resources/ResidentResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ResidentResource\Pages;
+use App\Filament\Resources\ResidentResource\RelationManagers;
+use App\Models\Resident;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ResidentResource extends Resource
+{
+    protected static ?string $model = Resident::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListResidents::route('/'),
+            'create' => Pages\CreateResident::route('/create'),
+            'edit' => Pages\EditResident::route('/{record}/edit'),
+        ];
+    }
+}
