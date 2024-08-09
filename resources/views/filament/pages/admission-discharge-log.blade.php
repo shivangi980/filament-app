8c269f0b726081472b69b4aa213b292e79aec97c
shivangi980 <113875231+shivangi980@users.noreply.github.com>
2024-08-09 15:24:04 +0530

    edit_delete

--- resources/views/filament/pages/admission-discharge-log.blade.php
+++ resources/views/filament/pages/admission-discharge-log.blade.php
@@ -1,39 +1,49 @@
 <x-filament::page>
     <h2>Admission/Discharge Log</h2>
     
-    <table class="table-auto w-full">
-        <thead>
-            <tr>
-                <th class="px-4 py-2">First Name</th>
-                <th class="px-4 py-2">Last Name</th>
-                <th class="px-4 py-2">Date Admitted</th>
-                <th class="px-4 py-2">Date of Birth</th>
-                <th class="px-4 py-2">SSN</th>
-                <th class="px-4 py-2">Place admitted from</th>
-                <th class="px-4 py-2">Special Notes</th>
-                <th class="px-4 py-2">Date of Discharge</th>
-                <th class="px-4 py-2">Discharge Reason</th>
-                <th class="px-4 py-2">Discharge Notes</th>
-                <th class="px-4 py-2">Discharged To</th>
-                <!-- Add more fields as necessary -->
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($this->getResidents() as $resident)
+    <div class="overflow-x-auto">
+        <table class="table-auto w-full">
+            <thead>
                 <tr>
-                    <td class="border px-4 py-2">{{ $resident->first_name }}</td>
-                    <td class="border px-4 py-2">{{ $resident->last_name }}</td>
-                    <td class="border px-4 py-2">{{ $resident->date_of_admission }}</td>
-                    <td class="border px-4 py-2">{{ $resident->dob }}</td>
-                    <td class="border px-4 py-2">{{ $resident->social_security }}</td>
-                    <td class="border px-4 py-2">{{ $resident->admitted_form }}</td>
-                    <td class="border px-4 py-2">{{ $resident->notes }}</td>
-                    <td class="border px-4 py-2">{{ $resident->date_of_discharge }}</td>
-                    <td class="border px-4 py-2">{{ $resident->discharge_reason }}</td>
-                    <td class="border px-4 py-2">{{ $resident->discharge_notes }}</td>
-                    <td class="border px-4 py-2">{{ $resident->discharged_to }}</td>
+                    <th class="px-4 py-2">First Name</th>
+                    <th class="px-4 py-2">Last Name</th>
+                    <th class="px-4 py-2">Date Admitted</th>
+                    <th class="px-4 py-2">Date of Birth</th>
+                    <th class="px-4 py-2">SSN</th>
+                    <th class="px-4 py-2">Place admitted from</th>
+                    <th class="px-4 py-2">Special Notes</th>
+                    <th class="px-4 py-2">Date of Discharge</th>
+                    <th class="px-4 py-2">Discharge Reason</th>
+                    <th class="px-4 py-2">Discharge Notes</th>
+                    <th class="px-4 py-2">Discharged To</th>
+                    <th class="px-4 py-2">Actions</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($this->getResidents() as $resident)
+                    <tr>
+                        <td class="border px-4 py-2">{{ $resident->first_name }}</td>
+                        <td class="border px-4 py-2">{{ $resident->last_name }}</td>
+                        <td class="border px-4 py-2">{{ $resident->date_of_admission }}</td>
+                        <td class="border px-4 py-2">{{ $resident->dob }}</td>
+                        <td class="border px-4 py-2">{{ $resident->social_security }}</td>
+                        <td class="border px-4 py-2">{{ $resident->admitted_form }}</td>
+                        <td class="border px-4 py-2">{{ $resident->notes }}</td>
+                        <td class="border px-4 py-2">{{ $resident->date_of_discharge }}</td>
+                        <td class="border px-4 py-2">{{ $resident->discharge_reason }}</td>
+                        <td class="border px-4 py-2">{{ $resident->discharge_notes }}</td>
+                        <td class="border px-4 py-2">{{ $resident->discharged_to }}</td>
+                        <td class="border px-4 py-2">
+                            <a href="{{ route('filament.resources.residents.edit', $resident) }}" class="text-blue-500">Edit</a>
+                            <form action="{{ route('filament.resources.residents.destroy', $resident) }}" method="POST" style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="text-red-500" onclick="return confirm('Are you sure?')">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </x-filament::page>
