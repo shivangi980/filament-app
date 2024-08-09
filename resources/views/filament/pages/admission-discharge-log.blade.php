fdbdf40ce601f9810ea64c2d4817024da13adeca
shivangi980 <113875231+shivangi980@users.noreply.github.com>
2024-08-09 19:09:42 +0530

    Update admission-discharge-log.blade.php

--- resources/views/filament/pages/admission-discharge-log.blade.php
+++ resources/views/filament/pages/admission-discharge-log.blade.php
@@ -2,7 +2,7 @@
     <h2>Admission/Discharge Log</h2>
     <div class="mb-4">
         <a href="{{ route('print-residents') }}" target="_blank" class="btn btn-primary">
-            Print List
+            <button>Print List</button>
         </a>
     </div>
     <div class="overflow-x-auto">
